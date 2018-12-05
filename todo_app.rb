1b3f1879efb3ccedf5e73a0db2c233e6aa075eb7
Derek <derek.baker89@gmail.com>
2018-12-05 16:04:31 +0000

    edit list item and ordered item list

--- todo_app.rb
+++ todo_app.rb
@@ -8,6 +8,10 @@ def create_list_item(todo)
 	TODO << todos
 end
 
+def edit_list_item(todo, index)
+	TODO[index.to_i - 1][:todo] = todo
+end
+
 get '/' do
 	erb :welcome
 end
@@ -16,8 +20,23 @@ get '/new_item' do
 	erb :new_item
 end
 
+get '/edit_item' do
+	erb :edit_item
+end
+
 post '/new_item' do
 	new_item = create_list_item(params[:todo])
 	redirect '/'
 end
+
+post '/edit_item' do
+	edit_list_item(params[:todo], params[:index])
+	redirect '/'
+end
+
+
+
+
+
+
 
